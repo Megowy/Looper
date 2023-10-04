26ae8826a2dd3b50ef1ce19a700c591ab6f6e9f5
megson <megowy@gmail.com>
2023-10-04 15:37:59 +0200

    Update URL endpoint in urls file

--- youtube_app/urls.py
+++ youtube_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import SearchResultsView, yt_player
 
 urlpatterns = [
-    path('youtube_app/', SearchResultsView.as_view(), name='youtube_search'),
-    path('youtube_app/<video_id>', yt_player, name='youtube_player'),
+    path('/', SearchResultsView.as_view(), name='youtube_search'),
+    path('/<video_id>', yt_player, name='youtube_player'),
 ]
